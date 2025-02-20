e6baa01c6ec0173a9ab105b1eeff041c2d565ef0
AdLKL <lkl88adnane@gmail.com>
2025-02-20 15:14:33 +0100

    Started the Products Page

--- ecommerce-multiv-frontend/src/App.tsx
+++ ecommerce-multiv-frontend/src/App.tsx
@@ -4,13 +4,15 @@ import Navbar from './customer/components/navbar/Navbar.tsx';
 import { ThemeProvider } from '@mui/material';
 import customTheme from './theme/CustomTheme.ts';
 import Home from './customer/pages/home/Home.tsx';
+import Product from './customer/pages/product/Product.tsx';
 
 function App() {
   return (
       <ThemeProvider theme={customTheme}>
         <div>
           <Navbar/>
-          <Home/>
+          {/* <Home/> */}
+          <Product/>
         </div>
       </ThemeProvider>
       
